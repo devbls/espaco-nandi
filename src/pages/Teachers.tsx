41ef29943dfc54d7db42fb9e873c1552764199d4
Brenno Santos <devblsds@gmail.com>
2024-11-19 00:34:12 -0300

    Centralize teachers title

--- src/pages/Teachers.tsx
+++ src/pages/Teachers.tsx
@@ -29,7 +29,7 @@ const TEACHERS = [
 export function Teachers() {
   return (
     <section className="min-h-[calc(100vh-6rem)] flex flex-col items-center py-20 px-12 font-poppins">
-      <h1 className="text-4xl font-bold">NOSSOS PROFESSORES</h1>
+      <h1 className="text-4xl font-bold text-center">NOSSOS PROFESSORES</h1>
       <div className="max-w-[1280px] flex flex-col gap-24 md:gap-16 mt-20">
         {TEACHERS.map((teacher, index) => (
           <div className={`flex ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'} flex-col gap-8 items-center`}>
